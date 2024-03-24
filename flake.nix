b97bfc49a9a939954dbe47a63c960b710b9f9c2d
Mikko Räsänen <1522239+mikkolukas@users.noreply.github.com>
2024-03-24 22:00:07 +0000

    Changed NixOS-WSL to flake

--- flake.nix
+++ flake.nix
@@ -3,6 +3,9 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    nixos-wsl.url = "github:nix-community/nixos-wsl";
+    nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
@@ -10,6 +13,8 @@
     nixosConfigurations.wsl = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
+        inputs.nixos-wsl.nixosModules.wsl
+        
         # Import the previous configuration.nix we used,
         # so the old configuration file still takes effect
         ./configuration.nix
