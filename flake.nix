933528b0fde842a0c50190805e77314f03ce796c
Mikko Räsänen <1522239+mikkolukas@users.noreply.github.com>
2024-03-25 00:37:48 +0000

    Added input entries

--- flake.nix
+++ flake.nix
@@ -1,24 +1,37 @@
 {
-  description = "A simple NixOS flake";
+  description = "Mikko Räsänen's NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-stable.url = github:nixos/nixpkgs/nixos-23.11;
+    nixos-unstable.url = github:NixOS/nixpkgs/nixos-unstable;
 
-    nixos-wsl.url = "github:nix-community/nixos-wsl";
-    nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
+    nixos.follows = "nixos-unstable";
+
+    # a collection of NixOS modules covering hardware quirks
+    nixos-hardware.url = github:NixOS/nixos-hardware/master;
+
+    home-manager.url = github:nix-community/home-manager;
+    home-manager.inputs.nixpkgs.follows = "nixos-unstable";
+
+    hyprland.url = github:hyprwm/Hyprland;
+
+    nixos-wsl.url = github:nix-community/NixOS-WSL;
+    nixos-wsl.inputs.nixpkgs.follows = "nixos";
   };
 
-  outputs = { self, nixpkgs, nixos-wsl, ... }@inputs: {
+  outputs = { self, nixos, home-manager, nixos-wsl, ... }@inputs: {
     # Please replace my-nixos with your hostname
-    nixosConfigurations.wsl = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        nixos-wsl.nixosModules.wsl
+    nixosConfigurations  = {
+      wsl = nixos.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          nixos-wsl.nixosModules.wsl
         
-        # Import the previous configuration.nix we used,
-        # so the old configuration file still takes effect
-        ./configuration.nix
-      ];
+          # Import the previous configuration.nix we used,
+          # so the old configuration file still takes effect
+          ./configuration.nix
+        ];
+      };
     };
   };
 }
